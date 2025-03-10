d64a28ce854990b018ef7982ec958647b4a58275
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-03-10 12:27:47 +0530

    Implemented router.route

--- routes/listings.js
+++ routes/listings.js
@@ -4,17 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
-router.get("/", wrapAsync(listingController.homePage));
+router.route("/")
+    .get(wrapAsync(listingController.homePage))
+    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
 
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.get("/:id", wrapAsync(listingController.showListing));
 
-router.post("/", validateListing, wrapAsync(listingController.createListing));
-
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
-
-router.put("/:id/edit", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.editListing));
+router.route("/:id/edit")
+    .get(isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm))
+    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.editListing));
 
 router.delete("/:id/delete", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
 
-router.get("/signup", userController.renderSignUpForm);
+router.route("/signup")
+    .get(userController.renderSignUpForm)
+    .post(wrapAsync(userController.signUpUser));
 
-router.post("/signup", wrapAsync(userController.signUpUser));
-
-router.get("/login", userController.renderLogInForm);
-
-router.post("/login", saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), wrapAsync(userController.logInUser));
+router.route("/login")
+    .get(userController.renderLogInForm)
+    .post(saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), wrapAsync(userController.logInUser));
 
 router.get("/logout", userController.logOutUser);
 
